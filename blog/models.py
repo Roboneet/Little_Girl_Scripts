2176f73a98dca0192099f98c08ae0f77ba50595e
Roboneet <neethumariyajoy@gmail.com>
2017-03-30 15:40:45 +0530

    approve or remove comment feature added

--- blog/models.py
+++ blog/models.py
@@ -19,11 +19,14 @@ class Post(models.Model):
 	def __str__(self):
 		return self.title
 
+	def approved_comments(self):
+		return self.comments.filter(approved_comment=True)
+
 class Comment(models.Model):
 	post = models.ForeignKey('blog.Post',related_name='comments')
 	author = models.CharField(max_length=200)
 	text = models.TextField()
-	created_date = models.DateTimeField(default=timezone.now())
+	created_date = models.DateTimeField(default  = timezone.now)
 	approved_comment = models.BooleanField(default=False)
 
 	def approve(self):
